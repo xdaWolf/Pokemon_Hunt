250efd216883098ca1cb42a7bc54fed2008d57c7
xdaWolf <sarah.holzmann@gmx.net>
2022-06-09 08:52:03 +0200

    Test

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main( int argc, char *argv[] )
+{
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow( "Hello SDL World", SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI, NULL );
+
+    if(window == NULL)
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_Event windowEvent;
+
+    while (true)
+    {
+        if ( SDL_PollEvent(&windowEvent) )
+        {
+            if (windowEvent.type == SDL_QUIT)
+            {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
