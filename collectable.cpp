57f39b466da644056d85d13e2cab71160935d767
NiklCode <95688637+NiklCode@users.noreply.github.com>
2022-07-28 20:25:23 +0200

    Version 1.0
    Version 1.0

--- collectable.cpp
+++ collectable.cpp
@@ -5,45 +5,12 @@
 #include <string>
 #include <stdlib.h>
 #include <chrono>
-<<<<<<< Updated upstream
-=======
 #include <windows.h>            //include all necessary external files
->>>>>>> Stashed changes
 
 
 
 Collectable::Collectable()      //constructor
 {
-<<<<<<< Updated upstream
-    std::cout << "Collectable Konstruktor" << std::endl;
-    auto start = std::chrono::steady_clock::now();
-    std::cout << "Enemy Konstruktor" << std::endl;
-    int dfe = 150;  //distance from edges (in pixels, maybe implement automatic number here using size of respective sprite)
-    int phss = 20;  //possible horizontal spawn spots (should be below lowest elapsed time and below average difference in elapsed times)          //hier müsste man noch ein zweiten phss erstellen, wenn shapeE und spriteE verschiedene Positionen haben sollen
-    int pvss = 5;   //possible vertical spawn spots   (                                       ~                                         )          //hier müsste man noch ein zweiten pvss erstellen, wenn shapeE und spriteE verschiedene Positionen haben sollen
-    auto end = std::chrono::steady_clock::now();
-    int microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-    std::cout << "Microseconds: " << microseconds << std::endl;
-    //SET VARIABLES - COLLECTABLE
-    this->textureC.loadFromFile("resources/collectable_" + std::to_string(microseconds % 6 + 1) + ".png");
-    this->textureC.setSmooth(true);
-    this->shapeC.setTexture(&textureC);
-    this->shapeC.setSize(sf::Vector2f(this->textureC.getSize().x, this->textureC.getSize().y));
-    this->spriteC.setTexture(textureC);
-    this->shapeC .setPosition(microseconds % phss * (int((1920 - 2*dfe) / (phss-1))) + dfe, microseconds % pvss * (int((1080 - 2*dfe) / (pvss-1))) + dfe);    //prefer fewer equally distributed spots across       //maybe implement automatic window size detection here
-    this->spriteC.setPosition(microseconds % phss * (int((1920 - 2*dfe) / (phss-1))) + dfe, microseconds % pvss * (int((1080 - 2*dfe) / (pvss-1))) + dfe);    //the map than many spots all close to each other
-};
-
-Collectable::~Collectable()
-{
-
-};
-
-sf::Sprite Collectable::getSprite()
-{
-    return this->spriteC;
-
-=======
     std::cout << "Collectable constructor" << std::endl;
     textureC.loadFromFile("resources/collectable_" + std::to_string(giveRandomNumber(1,6)) + ".png");   //select random Pokemon image
     textureC.setSmooth(true);
@@ -72,5 +39,4 @@ int Collectable::giveRandomNumber(int pMin, int pMax)
     {
         return microseconds % (pMax - 2*dfe) + dfe;     //select random position for enemy to spawn (considering the set distance from edges)
     }
->>>>>>> Stashed changes
 }
--- player.cpp
+++ player.cpp
@@ -6,33 +6,6 @@
 #include <stdlib.h>
 #include <string.h>     //include all necessary external files
 
-<<<<<<< Updated upstream
-Player::Player()
-{
-    //SET VARIABLES - PLAYER
-    this->speed = 19;
-    this->hp = 3;
-
-    //VISUALS - PLAYER
-    this->shapePPos = sf::Vector2f(1920 / 2 - 114 / 2, 1080 / 2 - 114 / 2);
-    std::cout << "Player Konstruktor" << std::endl;
-    this->textureP.loadFromFile("resources/pikachu_5.png");
-    this->textureP.setSmooth(true);
-    this->shapeP.setTexture(&textureP);
-    this->shapeP.setSize(sf::Vector2f(this->textureP.getSize().x, this->textureP.getSize().y));
-    this->spriteP.setTexture(textureP);
-    this->shapeP.setPosition(shapePPos);
-    this->spriteP.setPosition(shapePPos);
-    this->spriteP.setOrigin(spriteP.getGlobalBounds().width / 2., spriteP.getGlobalBounds().height / 2.);
-    
-
-};
-
-Player::~Player()
-{
-    //delete this->field;
-};
-=======
 
 Player::Player()        //constructor
 {
@@ -49,35 +22,17 @@ Player::Player()        //constructor
 };
 
 Player::~Player(){};    //destructor
->>>>>>> Stashed changes
 
 void Player::setTexture(const std::string& pTexture)        //change texture of player
 {
-    this->textureP.loadFromFile(pTexture);
-    this->spriteP.setTexture(textureP);
+    textureP.loadFromFile(pTexture);
+    spriteP.setTexture(textureP);
+    shapeP.setSize(sf::Vector2f(textureP.getSize().x, textureP.getSize().y));
+    shapeP.setTexture(&textureP);
 }
 
-<<<<<<< Updated upstream
-int Player::getHealthPoints()
-{
-    return this->hp;
-}
-
-void Player::setHealthPoints(int pHp)
-{
-    this->hp = pHp;
-}
-
-int Player::getSpeed()
-{
-    return this->speed;
-}
-
-
-=======
 int Player::getHealthPoints() { return hp; }                //get health points
 void Player::setHealthPoints(int pHp) { hp = pHp; }         //set health points
->>>>>>> Stashed changes
 
 int Player::getCollected() { return collected; }            //get amount of collected Pokemon
 void Player::setCollected(int pC) { collected = pC; }       //set amount of collected Pokemon
